92e814297d2b1f9c48c277889c372d9d8848d051
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-27 14:29:59 +0500

    creating login request and response

--- FYP/FrontEnd/Automation/server.js
+++ FYP/FrontEnd/Automation/server.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/login', (req,res) => {
+  console.log(req.body)
+  res.send({ "auth": 'success' });
+
+})
+
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
