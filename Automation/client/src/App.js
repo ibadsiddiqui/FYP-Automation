be0ab1452ece13168d645df91294019fe18fc727
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-28 13:40:13 +0500

    App.js refactored

--- Automation/client/src/App.js
+++ Automation/client/src/App.js
@@ -1,32 +1,17 @@
 import React, { Component } from 'react';
-
+import { Route } from "react-router-dom";
 import Login from './Component/Login'
+import Register from './Component/Register'
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
-
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
   render() {
     return (
-      <Login/>
+      <div>
+        <Route exact path="/" component={Login} />
+        <Route path="/register" component={Register} />
+      </div>
     );
   }
 }
