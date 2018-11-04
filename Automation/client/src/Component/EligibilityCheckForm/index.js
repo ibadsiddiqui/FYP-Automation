4cd125c729f980954b66147ca70351386eb2b8df
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-04 19:03:37 +0500

    Form for  hecking eligibility

---
+++ Automation/client/src/Component/EligibilityCheckForm/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default class EligibilityCheckForm extends Component {
+    render(){
+        return(
+            <div className="class">
+                
+            </div>
+        )
+    }
+}
