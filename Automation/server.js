821459f7be1669dd2cad26d2b68e265f92a49aa9
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-07 00:06:18 +0500

    refactored to controllers

--- Automation/server.js
+++ Automation/server.js
@@ -5,10 +5,27 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+
+const LoginAuthController = require('./controllers/Auth/Login')
+const RegisterAuthController = require('./controllers/Auth/Register')
+const GetUsernameAuthController = require('./controllers/UserInfo/GetUsername')
+
+
 mongoose.connect('mongodb://localhost/Automation');
-const User = require('./models/User/User')
 const port = process.env.PORT || 4000;
 
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// registration process
+
+
+app.post('/login', LoginAuthController);
+app.post('/register', RegisterAuthController);
+app.post('/getusername', GetUsernameAuthController)
 
 
 
@@ -21,112 +38,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// registration process
-app.post('/register', async (request, response) => {
-  console.log(request.body)
-  const name = request.body.name;
-  const username = request.body.username;
-  const email = request.body.email;
-  const password = request.body.password;
-
-  var registered;
-
-  await User.getUserByUsername(username, (err, user) => {
-    if (err) throw err;
-    if (user === null) {
-      registered = false
-    } else {
-      registered = true;
-    }
-
-    console.log(registered)
-
-    if (registered === true) {
-
-      // meaning user is already registered by that username
-      response.send({ registration: false })
-
-    } else if (registered === false) {
-
-      // following steps registers a new user and sends response
-      const newUser = new User({
-        name: name,
-        username: username,
-        email: email,
-        password: password
-      });
-
-      User.registerUser(newUser, (err, user) => {
-        if (err)
-          response.send({ registration: "Error in creating new user. Please try again" })
-        response.send({ registration: true })
-      })
-    }
-  });
-
-})
-
-app.post('/getusername', (request, response) => {
-  const username = request.body.username;
-  User.getUserByUsername(username, (err, user) => {
-    if (err) throw err;
-    if (user !== null) {
-      isRegistered = true
-    } else {
-      isRegistered = false;
-    }
-    response.send({response: isRegistered})
-
-  })
-})
-
-app.post('/login', (request, response) => {
-
-  const username = request.body.username;
-  const password = request.body.password;
-
-
-  var isRegistered;
-  User.getUserByUsername(username, (err, user) => {
-    if (err) throw err;
-    if (user !== null) {
-      isRegistered = true
-    } else {
-      isRegistered = false;
-    }
-
-    console.log(isRegistered)
-
-    if (isRegistered == true) {
-
-      User.comparePassword(password, user.password, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) {
-          response.send({ 
-            message: "Password Matched",
-            auth: true
-          })
-        } else {
-          response.send({ 
-            message: "Wrong Password",
-            auth: false,
-          })
-        }
-      })
-    } else {
-      response.send({ 
-        message: 'Not registered',
-        auth: false
-      })
-    }
-  });
-
-})
-
 app.listen(port, () =>
   console.log(`Listening on port ${port}`)
 );
