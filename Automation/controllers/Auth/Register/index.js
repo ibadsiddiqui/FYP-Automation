43f82541368468bc7a62c0917ce15eadf8b15195
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-18 00:08:38 +0500

    Register Controller refactored with JWT

--- Automation/controllers/Auth/Register/index.js
+++ Automation/controllers/Auth/Register/index.js
@@ -1,33 +1,20 @@
+var jwt = require('jsonwebtoken');
+var config = require('./../../Secret')
+
 const User = require('./../../../models/User/User')
 
 module.exports = async (request, response) => {
-    console.log(request.body)
     const name = request.body.name;
     const username = request.body.username;
     const email = request.body.email;
     const password = request.body.password;
     const profession = request.body.profession;
 
-    var registered;
-    console.log(request.body)
+    var registered; // flag
+
     await User.getUserByUsername(username, (err, user) => {
         if (err) throw err;
         if (user === null) {
-            registered = false
-        } else {
-            registered = true;
-        }
-
-        console.log(registered)
-
-        if (registered === true) {
-
-            // meaning user is already registered by that username
-            response.send({ registration: false })
-
-        } else if (registered === false) {
-
-            // following steps registers a new user and sends response
             const newUser = new User({
                 _id: username,
                 name: name,
@@ -40,9 +27,15 @@ module.exports = async (request, response) => {
             User.registerUser(newUser, (err, user) => {
                 if (err)
                     response.send({ registration: "Error in creating new user. Please try again" })
-                response.send({ registration: true })
+                var token = jwt.sign({ id: user._id }, config.secret, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+                response.status(200).send({ registration: true, token: token });
             })
+        } else {
+            response.send({ registration: false })
         }
+
     });
 
 }  
