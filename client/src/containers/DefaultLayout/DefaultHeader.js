662e3864001b825b8411fc2463b8c49b39c9026f
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-21 11:37:19 +0500

    sign out option added

--- client/src/containers/DefaultLayout/DefaultHeader.js
+++ client/src/containers/DefaultLayout/DefaultHeader.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom'
 
-import { AppAsideToggler, AppHeaderDropdown,  AppSidebarToggler } from '@coreui/react';
+import { AppAsideToggler, AppHeaderDropdown, AppSidebarToggler } from '@coreui/react';
 
 const propTypes = {
   children: PropTypes.node,
@@ -11,36 +12,52 @@ const propTypes = {
 const defaultProps = {};
 
 class DefaultHeader extends Component {
+  constructor() {
+    super();
+    this.logOut = this.logOut.bind(this)
+    this.state = {
+      logout: false
+    }
+  }
+  logOut() {
+    localStorage.setItem('token', null);
+    this.setState({
+      logout: true
+    })
+  }
   render() {
+    if (this.state.logout === true)
+      return <Redirect to="login" from='/:id' />
 
-    // eslint-disable-next-line
-    const { children } = this.props;
+    if (this.state.logout === false) {
+      return (
 
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-        
-        <Nav className="ml-auto" navbar>
+        <React.Fragment>
+          <AppSidebarToggler className="d-lg-none" display="md" mobile />
+          <AppSidebarToggler className="d-md-down-none" display="lg" />
 
-          <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">1</Badge></NavLink>
-          </NavItem>
-          <AppHeaderDropdown direction="down">
+          <Nav className="ml-auto" navbar>
 
-            <DropdownToggle nav>
-              <img src={'assets/img/avatars/user.png'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-            </DropdownToggle>
+            <NavItem className="d-md-down-none">
+              <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">1</Badge></NavLink>
+            </NavItem>
+            <AppHeaderDropdown direction="down">
 
-            <DropdownMenu right style={{ right: 'auto' }}>
-              <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
-            </DropdownMenu>
-          </AppHeaderDropdown>
-        </Nav>
-        <AppAsideToggler className="d-md-down-none" />
-        {/*<AppAsideToggler className="d-lg-none" mobile />*/}
-      </React.Fragment>
-    );
+              <DropdownToggle nav>
+                <img src={'assets/img/avatars/user.png'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+              </DropdownToggle>
+
+              <DropdownMenu right style={{ right: 'auto' }}>
+
+                <DropdownItem onClick={this.logOut}><i className="fa fa-lock" ></i> Logout</DropdownItem>
+              </DropdownMenu>
+            </AppHeaderDropdown>
+          </Nav>
+          <AppAsideToggler className="d-md-down-none" />
+          {/*<AppAsideToggler className="d-lg-none" mobile />*/}
+        </React.Fragment>
+      )
+    }
   }
 }
 
--- models/User/User.js
+++ models/User/User.js
@@ -55,7 +55,7 @@ module.exports.getUserByProfession = function (profession, callback) {
 
 module.exports.getUserById = function (id, callback) {
     User.findById(id, callback);
-}
+}   
 
 module.exports.comparePassword = function (candidatePassword, hash, callback) {
     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
