e454824aa89401fdc5561d1c4156d602c28a695d
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-29 20:50:12 +0500

    file refactored

--- server.js
+++ server.js
@@ -4,23 +4,8 @@ const mongoose = require('mongoose');
 const expressEdge = require('express-edge');
 
 const express = require('express');
-const bodyParser = require('body-parser')
-
 const app = express();
-const Project = require('./models/Projects/Projects')
-
-
-const LoginAuthController = require('./controllers/Auth/Login')
-const RegisterAuthController = require('./controllers/Auth/Register')
-const GetUsernameAuthController = require('./controllers/UserInfo/GetDetails')
-const UpdateUserProfileController = require('./controllers/UserInfo/UpdateUserProfileController')
-
-
-const adminManageEligibilities = require('./controllers/Admin/ManageEligibilities')
-const adminManageUsers = require('./controllers/Admin/ManageUsers');
-const adminManageProjects = require('./controllers/Admin/ManageProjects')
-
-
+const bodyParser = require('body-parser')
 mongoose.connect('mongodb://localhost/Automation');
 const port = process.env.PORT || 4000;
 
@@ -37,39 +22,31 @@ app.set('views', `${__dirname}/views`);
 
 // ////////////////////////////////////////admin works
 
-app.get('/', (request, response) => {
 
-  response.render('index')
-})
+
+const LoginAuthController = require('./controllers/Auth/Login')
+const RegisterAuthController = require('./controllers/Auth/Register')
+const GetUsernameAuthController = require('./controllers/UserInfo/GetDetails')
+const UpdateUserProfileController = require('./controllers/UserInfo/UpdateUserProfileController')
+const createNewProjectController = require('./controllers/Project/createNewProjectControllers')
+
+const adminDashboard = require('./controllers/Admin/Dashboard')
+const adminManageEligibilities = require('./controllers/Admin/ManageEligibilities')
+const adminManageUsers = require('./controllers/Admin/ManageUsers');
+const adminManageProjects = require('./controllers/Admin/ManageProjects')
+const adminManageFYPList = require('./controllers/Admin/adminManageFYPList')
+
+
+
+app.get('/',  adminDashboard)
 
 app.get('/manageusers', adminManageUsers)
 
-app.get('/fyplist', (request, response) => {
-  response.render('fyplist')
-})
+app.get('/fyplist', adminManageFYPList)
 
 app.get('/manageeligibilities', adminManageEligibilities)
 app.get('/manageprojects', adminManageProjects)
-app.post('/insertProject', async (req, res) => {
-  const name = req.body.pname;
-  const groupid = req.body.groupid;
-  const status = req.body.status;
-  const abstract = req.body.abstract;
-  const createdAt = new Date().toLocaleString();
-  const completedAt = req.body.completedAt;
-  const newProject = new Project({
-    project_name: name,
-    group_id: groupid,
-    status: status,
-    abstract: abstract,
-    createdAt: createdAt,
-    completedAt: completedAt
-  })
-  Project.create(newProject, (err, project) => {
-    if (err) throw err;
-    console.log('New Project', project)
-  })
-})
+app.post('/insertProject', createNewProjectController)
 
 
 
