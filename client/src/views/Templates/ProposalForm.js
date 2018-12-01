8da41b007d4983abec72d4418f28368515624893
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-02 00:25:25 +0500

    ProposalForm component updated to full functional stateful component

--- client/src/views/Templates/ProposalForm.js
+++ client/src/views/Templates/ProposalForm.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { 
-    Card, 
-    CardBody, 
-    CardHeader, 
-    Col, 
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
     FormGroup,
     Input,
     CardFooter,
-    Button
+    Button,
 } from 'reactstrap';
 
 
@@ -15,81 +15,300 @@ export default class ProposalForm extends Component {
 
     constructor(props) {
         super(props);
-
-        // this.toggle = this.toggle.bind(this);
-        // this.state = {
-        //     activeTab: 0
-        // };
+        this.initializeState = this.initializeState.bind(this)
+        this.state = {}
+    }
+    componentDidMount(){
+        this.initializeState()
+    }
+    initializeState(){
+        this.state = {
+            program_of_study: '',
+            student_year_session: '',
+            student_enrollment_year: '',
+            student_CMS_ID: '',
+            project_name: '',
+            problem_statement: '',
+            motivation: '',
+            objective: '',
+            literature_review: '',
+            scope: '',
+            methodology: '',
+            useCaseDiagram: '',
+            raciChart: '',
+            supervisor_fullname: '',
+            supervisor_designation: '',
+            co_supervisor_fullname: "",
+            co_supervisor_designation: '',
+            external_supervisor_fullname: '',
+            external_supervisor_designation: '',
+            proposal_submitted_At: ''
+        }
     }
 
+
+    setProgramOfStudy(event) {
+        this.setState({
+            program_of_study: event.target.value
+        })
+    }
+
+    setStudentEnrollmentYear(event) {
+        this.setState({
+            student_enrollment_year: event.target.value
+        })
+    }
+    setStudentYearSession(event) {
+        this.setState({
+            student_year_session: event.target.value
+        })
+    }
+    setStudentCMSID(event) {
+        this.setState({
+            student_CMS_ID: event.target.value
+        })
+    }
+
+    setProjectName(event) {
+        this.setState({
+            project_name: event.target.value
+        })
+    }
+    setProblemStatement(event) {
+        this.setState({
+            problem_statement: event.target.value
+        })
+    }
+    setMotivation(event) {
+        this.setState({
+            motivation: event.target.value
+        })
+    }
+    setObjective(event) {
+        this.setState({
+            objective: event.target.value
+        })
+    }
+    setLiteratureReview(event) {
+        this.setState({
+            literature_review: event.target.value
+        })
+    }
+    setScope(event) {
+        this.setState({
+            scope: event.target.value
+        })
+    }
+    setMethodology(event) {
+        this.setState({
+            methodology: event.target.value
+        })
+    }
+
+    uploadUserCase(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file)
+        setTimeout(() => {
+            if (reader.readyState === 2) {
+                this.setState({
+                    useCaseDiagram: reader.result
+                });
+            }
+        }, 500)
+    }
+    uploadRACIChart(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file)
+        setTimeout(() => {
+            if (reader.readyState === 2) {
+                this.setState({
+                    raciChart: reader.result
+                });
+                console.log(this.state)
+            }
+        }, 500)
+    }
+
+    setSupervisorName(event) {
+        this.setState({
+            supervisor_fullname: event.target.value
+        })
+    }
+    setSupervisorDesignation(event) {
+        this.setState({
+            supervisor_designation: event.target.value
+        })
+    }
+    setCoSupervisorName(event) {
+        this.setState({
+            co_supervisor_fullname: event.target.value
+        })
+    }
+    setCoSupervisorDesignation(event) {
+        this.setState({
+            co_supervisor_designation: event.target.value
+        })
+    }
+    setExSupervisorName(event) {
+        this.setState({
+            external_supervisor_fullname: event.target.value
+        })
+    }
+    setExSupervisorDesignation(event) {
+        this.setState({
+            external_supervisor_designation: event.target.value
+        })
+    }
+
+    onSubmit() {
+        console.log(this.state)
+    }
     render() {
         return (
             <Card>
-              <CardHeader>
-                <strong>Proposal</strong>
-                <small> Form</small>
-              </CardHeader>
-              <CardBody>
-                <FormGroup>
-                  <small htmlFor="company">Program of Study</small>
-                  <Input type="text" id="programName" placeholder="Enter your program of study" />
-                </FormGroup>
-                <FormGroup row className="my-0">
-                <Col xs="6">
-                        <FormGroup>
-                            <small htmlFor="city">Year Session</small>
-                            <Input type="text" id="yearSession" placeholder="Enter your session: Fall or Spring" />
-                        </FormGroup>
-                    </Col>
-                    <Col xs="6">
-                        <FormGroup>
-                        <small htmlFor="postal-code">Enrollment Year</small>
-                        <Input type="text" id="postal-code" placeholder="Enter your year of Enrollment" />
-                        </FormGroup>
-                    </Col>
-                </FormGroup>
+                <CardHeader>
+                    <strong>Proposal</strong>
+                    <small> Form</small>
 
-                <FormGroup>
-                  <small htmlFor="street">Project Name</small>
-                  <Input type="text" id="projectName" placeholder="Enter Your Project Name" />
-                </FormGroup>
-                
-                <strong>Students Details: (Minimum 1 is requried)</strong>
+                </CardHeader>
+                <CardBody>
+                    <strong> This form should be filled only once and will only be available if the project gets rejected</strong>
+                    <FormGroup>
+                        <small htmlFor="programName">Program of Study</small>
+                        <Input type="text" defaultValue={this.state.program_of_study} id="programName" onChange={(event) => this.setProgramOfStudy(event)} placeholder="Enter your program of study" />
+                    </FormGroup>
+                    <FormGroup row className="my-0">
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="city">Year Session</small>
+                                <Input type="text" defaultValue={this.state.student_year_session} id="yearSession" placeholder="Enter your session: Fall or Spring" onChange={(event) => this.setStudentYearSession(event)} />
+                            </FormGroup>
+                        </Col>
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="postal-code">Enrollment Year</small>
+                                <Input type="text" value={this.state.student_enrollment_year} id="postal-code" placeholder="Enter your year of Enrollment" onChange={(event) => this.setStudentEnrollmentYear(event)} />
+                            </FormGroup>
+                        </Col>
+                    </FormGroup>
 
-                <FormGroup row className="my-0">
-                    <Col xs="8">
-                        <FormGroup>
-                            <small htmlFor="city">Student Full Name</small>
-                            <Input type="text" id="city" placeholder="Enter your full name" />
-                        </FormGroup>
-                    </Col>
-                    <Col xs="4">
-                        <FormGroup>
-                            <small htmlFor="postal-code">CMS ID</small>
-                            <Input type="text" id="postal-code" placeholder="Enter your CMS ID" />
-                        </FormGroup>
-                    </Col>
-                </FormGroup>
-                <FormGroup row className="my-0">
-                    <Col xs="6">
-                        <FormGroup>
-                            <small htmlFor="city">Email</small>
-                            <Input type="text" id="city" placeholder="Enter your full name" />
-                        </FormGroup>
-                    </Col>
-                    <Col xs="6">
-                        <FormGroup>
-                            <small htmlFor="postal-code">CMS ID</small>
-                            <Input type="text" id="postal-code" placeholder="Enter your CMS ID" />
-                        </FormGroup>
-                    </Col>
-                </FormGroup>
-              </CardBody>
+                    <FormGroup>
+                        <small htmlFor="street">Project Name</small>
+                        <Input type="text" value={this.state.project_name} id="projectName" placeholder="Enter Your Project Name" onChange={(event) => this.setProjectName(event)} />
+                    </FormGroup>
 
-              <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter>
+                    <strong>Students Details</strong>
+
+                    <FormGroup>
+                        <small htmlFor="postal-code">CMS ID</small>
+                        <Input type="text" value={this.state.student_CMS_ID} id="postal-code" placeholder="Enter your CMS ID" onChange={(event) => this.setStudentCMSID(event)} />
+                    </FormGroup>
+
+                    <strong>Project Details</strong>
+                    <FormGroup>
+                        <small htmlFor="problemStatement">Problem Statement</small>
+                        <div className="form-group">
+                            <textarea className="form-control" value={this.state.problem_statement} id="problemStatement" rows="5" placeholder="Enter your project's problem statement" onChange={(event)=> this.setProblemStatement(event)}></textarea>
+                        </div>
+                    </FormGroup>
+                    <FormGroup>
+                        <small htmlFor="motivation">Motivation</small>
+                        <div className="form-group">
+                            <textarea className="form-control" id="motivation" rows="5" value={this.state.motivation} placeholder="Enter your motivation for the project" onChange={(event) => this.setMotivation(event)}></textarea>
+                        </div>
+                    </FormGroup>
+                    <FormGroup>
+                        <small htmlFor="objective">Objective</small>
+                        <div className="form-group">
+                            <textarea className="form-control" id="objective" rows="5" value={this.state.objective} placeholder="Enter your objectives of project" onChange={(event) => this.setObjective(event)}></textarea>
+                        </div>
+                    </FormGroup>
+                    <FormGroup>
+                        <small htmlFor="literatureReview">Literature Review</small>
+                        <div className="form-group">
+                            <textarea className="form-control" id="literatureReview" rows="5" value={this.state.literature_review} placeholder="Enter your literature review on project" onChange={(event) => this.setLiteratureReview(event)}></textarea>
+                        </div>
+                    </FormGroup>
+                    <FormGroup>
+                        <small htmlFor="scope">Scope of the Project</small>
+                        <div className="form-group">
+                            <textarea className="form-control" id="scope" rows="5" value={this.state.scope} placeholder="Enter your scope of the project" onChange={(event) => this.setScope(event)}></textarea>
+                        </div>
+                        <div className="wrap-input100 validate-input bg1 rs1-wrap-input100">
+                            <span className="label-input100">Upload Use Case or Process Flow Diagram:</span>
+                            <div className="col-xs-3">
+                                <input type="file" name="profilepic" onChange={(file) => this.uploadUserCase(file)} />
+                            </div>
+                        </div>
+
+                    </FormGroup>
+                    <FormGroup>
+                        <small htmlFor="methodology">Select Methodology</small>
+                        <Input type="select" name="select" id="methodology">
+                            <option>Agile</option>
+                            <option>Waterfall</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup> 
+                        <div className="wrap-input100 validate-input bg1 rs1-wrap-input100">
+                            <span className="label-input100">Upload RACI Chart Diagram:</span>
+                            <div className="col-xs-3">
+                                <input type="file" name="RACIchart" onChange={(event) => this.uploadRACIChart(event)} />
+                            </div>
+                        </div>
+                    </FormGroup>
+                    <strong>Supervisor Details</strong>
+                    <FormGroup row className="my-0">
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="supervisorName">Supervisor Name</small>
+                                <Input type="text" value={this.state.supervisor_fullname} id="supervisorName" placeholder="Enter your supervisor's full name" onChange={(event) => this.setSupervisorName(event)} />
+                            </FormGroup>
+                        </Col>
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="supervisorDesignation">Designation</small>
+                                <Input type="text" value={this.state.supervisor_designation} id="supervisorDesignation" placeholder="Enter supervisor's designation" onChange={(event) => this.setSupervisorDesignation(event)}/>
+                            </FormGroup>
+                        </Col>
+                    </FormGroup>
+                    <strong>Co-Supervisor (Otpional)</strong>
+                    <FormGroup row className="my-0">
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="co-supervisorName">Co-Supervisor Name</small>
+                                <Input type="text" value={this.state.co_supervisor_fullname} id="co-supervisorName" placeholder="Enter your co-supervisor's full name" onChange={(event) => this.setCoSupervisorName(event)}/>
+                            </FormGroup>
+                        </Col>
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="co-supervisorDesignation">Designation</small>
+                                <Input type="text" value={this.state.co_supervisor_designation} id="co-supervisorDesignation" placeholder="Enter co-supervisor's designation" onChange={(event) => this.setCoSupervisorDesignation(event)}/>
+                            </FormGroup>
+                        </Col>
+                    </FormGroup>
+                    <strong>For External Supervisor: (optional)</strong>
+                    <FormGroup row className="my-0">
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="Ex-supervisorName">External Supervisor Name</small>
+                                <Input type="text" value={this.state.external_supervisor_fullname} id="Ex-supervisorName" placeholder="Enter your external supervisor's full name" onChange={(event) => this.setExSupervisorName(event)}/>
+                            </FormGroup>
+                        </Col>
+                        <Col xs="6">
+                            <FormGroup>
+                                <small htmlFor="co-supervisorDesignation">Designation</small>
+                                <Input type="text" value={this.state.external_supervisor_designation} id="co-supervisorDesignation" placeholder="Enter external supervisor's designation" onChange={(event) => this.setExSupervisorDesignation(event)}/>
+                            </FormGroup>
+                        </Col>
+                    </FormGroup>
+                </CardBody>
+
+                <CardFooter>
+                    <Button type="submit" size="sm" color="primary" onClick={this.onSubmit.bind(this)}><i className="fa fa-dot-circle-o"></i> Submit</Button>
+                </CardFooter>
             </Card>
         );
     }
