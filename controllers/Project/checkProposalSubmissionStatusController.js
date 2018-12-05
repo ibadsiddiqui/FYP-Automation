bd038556e2f1d68a130178fcba5273fcf77c0413
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-05 23:55:07 +0500

    file name updated

---
+++ controllers/Project/checkProposalSubmissionStatusController.js
@@ -0,0 +1,35 @@
+const User = require('../../models/User/User')
+const decodedJWT = require('../../Helpers/JWTDecoder')
+
+module.exports = async (request, response) => {
+    const username = decodedJWT(request.headers.authorization)
+
+    if (username) {
+        await User.checkProposalSubmissionStatus(username, (err, user) => {
+            if (err) throw err;
+            if (user !== null) {
+                if (user.hasSubmittedProposal) {
+                    response.status(200).send({
+                        proposalSubmitted: true,
+                        hasSubmittedProposal: user.hasSubmittedProposal,
+                        hasProposalBeenAccepted: user.hasProposalBeenAccepted,
+                        hasSubmittedProgressReport: user.hasSubmittedProgressReport,
+                        hasSubmittedFinalReport: user.hasSubmittedFinalReport,
+                    })
+                }
+                else if (!user.hasSubmittedProposal) {
+                    response.status(200).send({
+                        proposalSubmitted: false,
+                        hasSubmittedProposal: user.hasSubmittedProposal,
+                        hasProposalBeenAccepted: user.hasProposalBeenAccepted,
+                        hasSubmittedProgressReport: user.hasSubmittedProgressReport,
+                        hasSubmittedFinalReport: user.hasSubmittedFinalReport,
+                    })
+                }
+            } else {
+                response.status(404).send({ proposalSubmitted: null })
+            }
+        })
+
+    }
+}
