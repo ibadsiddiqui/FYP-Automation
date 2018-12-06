cfd5af0547d4961ea1bf040198e834d87bf5c0aa
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-07 01:13:28 +0500

    limit for json uploading increased

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static('public'))
 app.use(expressEdge);
 
 // allow to accept and transfer data in json
+app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
