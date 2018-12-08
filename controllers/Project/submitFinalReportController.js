82e5e2152cdb3c1a09d50ec60099baffe591a7f2
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-08 17:23:21 +0500

    controller for final report submission added

--- controllers/Project/submitFinalReportController.js
+++ controllers/Project/submitFinalReportController.js
@@ -5,14 +5,13 @@ const decodedJWT = require('./../../Helpers/JWTDecoder')
 module.exports = async (req, res) => {
     const username = decodedJWT(req.headers.authorization);
     const progressReport = req.body.progressReport;
-    console.log(req.body)
     User.getUserById(username, async (err, user) => {
         if (err) throw err;
         if (user !== null) {
-            await User.updateProgressReportStatus(username, true, async (err, user) => {
+            await User.updateSubmittedFinalReportStatus(username, true, async (err, user) => {
                 if(err) throw err;
                 if(user.hasSubmittedProgressReport) {
-                    await Project.updateSubmissionOfProgressReport(user.username, progressReport, (err, updatedReport) => {
+                    await Project.updateSubmissionOfFinalReport(user.username, progressReport, (err, updatedReport) => {
                         if(err) throw err;
                         if(updatedReport !== null){
                             console.log(updatedReport)
