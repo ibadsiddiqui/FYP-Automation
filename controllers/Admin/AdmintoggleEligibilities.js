9f20adffd6ad6fe021e2d33223db7b9460e8b888
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-11 01:04:24 +0500

    eligility toggler added

--- controllers/Admin/AdmintoggleEligibilities.js
+++ controllers/Admin/AdmintoggleEligibilities.js
@@ -1,24 +1,26 @@
 const Form = require('../../models/Forms/EligibilityForm')
 
 module.exports = async (req, res) => {
-    console.log(req.body)
-    if (req.body['status'] === 'rejected') {
-        await Form.toggleStatus(req.body['id'], 'rejected', (err, updatedStatus) => {
-            if(err) throw err;
-            if(updatedStatus !== null) {
-                console.log(updatedStatus);
-                res.render('index')
-            }
-        })
-    } else {
-        await Form.toggleStatus(req.body['id'], 'accepted', (err, updatedStatus) => {
-            if(err) throw err;
-            if(updatedStatus !== null) {
-                console.log(updatedStatus);
-                res.render('index')
-            }
-        })
-
-    }
-
+    const value = await Form.findById({ _id: req.body['id'] })
+    if (value.status == "pending") {
+        console.log(value)
+        if (req.body['status'] === 'rejected') {
+            await Form.toggleStatus(req.body['id'], 'rejected', (err, updatedStatus) => {
+                if (err) throw err;
+                if (updatedStatus !== null) {
+                    console.log(updatedStatus);
+                    res.render('index')
+                }
+            })
+        } else {
+            await Form.toggleStatus(req.body['id'], 'accepted', (err, updatedStatus) => {
+                if (err) throw err;
+                if (updatedStatus !== null) {
+                    console.log(updatedStatus);
+                    res.render('index')
+                }
+            })
+    
+        }
+    } 
 }
