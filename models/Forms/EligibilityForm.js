f25650709afb127de8b43093bcc323ed2e612318
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-10 23:54:37 +0500

    status updated

--- models/Forms/EligibilityForm.js
+++ models/Forms/EligibilityForm.js
@@ -21,14 +21,34 @@ const EligibilityFormSchema = mongoose.Schema({
             type: String
         },
     },
+    status: {
+        type: String,
+        default: 'pending',
+    },
+    submittedAt: {
+        type: String,
+        default: new Date().toLocaleString()
+
+    }
 });
 
 const EligibilityForm = module.exports = mongoose.model('EligibilityForm', EligibilityFormSchema);
 
-module.exports.submitProposal = function (newForm, callback) {
-    EligibilityForm.create(newProposal, callback)
+module.exports.submitForm = function (newForm, callback) {
+    EligibilityForm.create(newForm, callback)
 }
 
-module.exports.getAllProposals = function (callback) {
+module.exports.getAllForms = function (callback) {
     EligibilityForm.find({}, callback);
+}
+
+module.exports.toggleStatus = function (formId, status, callback) {
+    EligibilityForm.findById(formId, (err, form) => {
+        if (err) throw (err);
+        if(form !== null){
+            form.set({ status: status });
+            form.save(callback);
+        }
+    });
+
 }
