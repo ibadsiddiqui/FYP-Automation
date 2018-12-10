9240bf42d2cebc8cc85aaef542e494e3ff95f064
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-11 01:06:49 +0500

    Request type fixed and status for checking eligibility added at registeration

--- controllers/UserInfo/GetDetails/index.js
+++ controllers/UserInfo/GetDetails/index.js
@@ -2,19 +2,34 @@ const User = require('./../../../models/User/User')
 const decodedJWT = require('./../../../Helpers/JWTDecoder')
 
 module.exports = (request, response) => {
-    const username = decodedJWT(request.headers.authorization) 
-
-    if (username){
-        // const username = request.body.username;
-        User.getUserByUsername( username, (err, user) => {
+    if (request.method === "POST") {
+        const username = request.body.username;
+        User.getUserByUsername(username, (err, user) => {
             if (err) throw err;
             if (user !== null) {
-                response.status(200).send({ response: true , user})
-    
+                response.status(200).send({ response: true, user })
+
             } else {
                 response.status(404).send({ response: false })
             }
         })
+    }
+    if (request.method === 'GET') {
+        const username = decodedJWT(request.headers.authorization)
+
+        if (username) {
+            // const username = request.body.username;
+            User.getUserByUsername(username, (err, user) => {
+                if (err) throw err;
+                if (user !== null) {
+                    response.status(200).send({ response: true, user })
+
+                } else {
+                    response.status(404).send({ response: false })
+                }
+            })
+
+        }
 
     }
 }
