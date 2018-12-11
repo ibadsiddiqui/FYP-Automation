f19f8bfbf53b2d37575c405601d14d5628290bb3
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-11 23:54:20 +0500

    console removed

--- controllers/Admin/AdmintoggleEligibilities.js
+++ controllers/Admin/AdmintoggleEligibilities.js
@@ -3,12 +3,10 @@ const Form = require('../../models/Forms/EligibilityForm')
 module.exports = async (req, res) => {
     const value = await Form.findById({ _id: req.body['id'] })
     if (value.status == "pending") {
-        console.log(value)
         if (req.body['status'] === 'rejected') {
             await Form.toggleStatus(req.body['id'], 'rejected', (err, updatedStatus) => {
                 if (err) throw err;
                 if (updatedStatus !== null) {
-                    console.log(updatedStatus);
                     res.render('index')
                 }
             })
@@ -16,7 +14,6 @@ module.exports = async (req, res) => {
             await Form.toggleStatus(req.body['id'], 'accepted', (err, updatedStatus) => {
                 if (err) throw err;
                 if (updatedStatus !== null) {
-                    console.log(updatedStatus);
                     res.render('index')
                 }
             })
