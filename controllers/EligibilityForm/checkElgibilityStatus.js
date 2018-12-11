49d959a359c023a8b8db72084ac5800c812a7980
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-11 23:05:27 +0500

    eligibility checkup updated

--- controllers/EligibilityForm/checkElgibilityStatus.js
+++ controllers/EligibilityForm/checkElgibilityStatus.js
@@ -1,17 +1,17 @@
 const Form = require('../../models/Forms/EligibilityForm')
 
 module.exports = async (req, res) => {
-    Form.checkStatus(req.body.cms_id ,(err, form)=>{
-        if(err) throw err;
-        if(form !== null) {
-            if(form.status === 'accepted'){
-                res.send({accepted: true, status: 'accepted'})
-            } else if(form.status === 'rejected'){
-                res.send({accepted: false, status: 'rejected'})
+    Form.checkStatus(req.body.cms_id, (err, form) => {
+        if (err) throw err;
+        if (form !== null) {
+            if (form.status === 'accepted') {
+                res.send({ accepted: true, status: 'accepted' })
+            } else if (form.status === 'rejected') {
+                res.send({ accepted: false, status: 'rejected' })
 
-            }else {
-                res.send({status: 'pending'})
-            } 
+            } else {
+                res.send({ status: 'pending' })
+            }
         }
     })
 }
