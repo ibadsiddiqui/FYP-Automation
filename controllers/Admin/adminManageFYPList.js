c70ff6798d026a41fd6d66ddc19c8c2eaeddad0e
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-12-12 23:10:04 +0500

    request handling added for removing or adding new projects under university

--- controllers/Admin/adminManageFYPList.js
+++ controllers/Admin/adminManageFYPList.js
@@ -1,5 +1,35 @@
-const Project = require('./../../models/Projects/Projects')
+const University_Projects = require('./../../models/Projects/UniversityProjects')
 module.exports = async (request, response) => {
-    const Projects = await Project.find({});
-    response.render('fyplist', {Projects})
+    if (request.method === "GET") {
+        const Projects = await University_Projects.find({});
+        response.render('fyplist', { Projects })
+
+    } else {
+        if (request.body.hasOwnProperty('project_name')) {
+
+            const model = {
+                project_name: request.body['project_name'],
+                abstract: request.body['project_abstract']
+            }
+            await University_Projects.createProject(model, (err, projectSubmitted) => {
+                if (err) throw err;
+                if (projectSubmitted) {
+                    response.render('index')
+                }
+            })
+        }else {
+            console.log(request.body['project_id'])
+            University_Projects.findByIdAndDelete(request.body['project_id'],(err, deleted) =>{
+                if(err) throw err;
+                if(deleted){
+                    console.log(deleted)
+                    response.render('index')
+                }else {
+                    response.render('index')
+                    
+                }
+            })
+
+        }
+    }
 }
