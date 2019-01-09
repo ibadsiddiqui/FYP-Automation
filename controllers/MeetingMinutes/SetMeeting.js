d0797cb60e067f9f2f05f51c17935b755549433f
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-10 01:32:29 +0500

    controller for creating new meetings added

---
+++ controllers/MeetingMinutes/SetMeeting.js
@@ -0,0 +1,49 @@
+const User = require('./../../models/User/User')
+const MeetingMinutes = require('./../../models/MeetingMinutes')
+const decodedJWT = require('../../Helpers/JWTDecoder')
+
+module.exports = async (req, res) => {
+
+    const username = decodedJWT(req.headers.authorization)
+
+    const model = {
+        meetingNumber: req.body.meetingNumber,
+        meetingTopic: req.body.meetingTopic,
+        meetingWith: req.body.meetingWith,
+        meetingDate: req.body.meetingDate,
+    }
+    if (username) {
+
+        await User.getUserById(username, async (err, user) => {
+            if (err) throw err;
+            if (user !== null) {
+
+                MeetingMinutes.getMeetingList(username, (err, details) => {
+                    if (err) throw err;
+                    if (details !== null) {
+
+                        details.Meetings.push(model);
+                        details.save();
+                        res.status(200).send(details)
+
+                    } else if (details === null || details === undefined) {
+
+                        const meetingModel = {
+                            _id: username,
+                            Meetings: [model]
+                        }
+                        
+                        MeetingMinutes.createMeeting(meetingModel, (err, newMeeting) => {
+                            if (err) throw err;
+                            if (!newMeeting) {
+                                res.status(200).send(true)
+                            }
+                        })
+                    }
+                })
+            }
+
+        })
+
+    }
+}
