49cc01a7658fac482552b1d6780bbb016adaed64
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-10 01:31:59 +0500

    meeting minutes form updated on the frontend

--- client/src/views/Teacher/Meeting_Minutes/index.js
+++ client/src/views/Teacher/Meeting_Minutes/index.js
@@ -1,123 +1,165 @@
 import React from 'react';
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Row,
+    Table,
+    Modal,
+    ModalHeader,
+    ModalBody
+} from 'reactstrap';
+
+
+
 
 class MeetingMinutes extends React.Component {
     constructor(props) {
         super(props);
+        this.toggleSuccessModal = this.toggleSuccessModal.bind(this)
         this.state = {
+            successModal: false,
+            list: [{
+                "meetingNumber": '#100123',
+                "meetingTopic": '#100123',
+                'meetingWith': '001-2014',
+                'meetingDate': new Date().toLocaleDateString(),
 
+            }],
+            correctInput: true,
         }
-    }
+        this.meetingTopic = "";
+        this.meetingWith = "";
+        this.meetingDate = ""
+        this.form = (
+            <div className="container">
+                <div className="wrap">
+                    <form className="contact-form validate-form">
 
-    render() {
-        return (
-            <div class="container-contact100">
-                <div class="wrap-contact100">
-                    <form class="contact100-form validate-form">
-                        <span class="contact100-form-title">
-                            Meeting Minute Form:
-				</span>
-                        <span class="contact100-form-title">
-                            <h6>To be completed by Supervisors:</h6>
-                        </span>
+                        {
+                            !(this.meetingDate || this.meetingWith || this.meetingTopic)
+                            &&
+                            <span className='text-info'>Please enter complete detalis</span>
+                        }
 
-
-
-
-                        <div class="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter Meeting Called By">
-                            <span class="label-input100">Meeting Called By:*</span>
-                            <input class="input100" type="text" name="name" placeholder=" Enter Meeting Called By: " />
+                        <div className="wrap-input100 validate-input bg0 rs1-alert-validate" data-validate="Please Type Your Discussion Summery">
+                            <span className="label-input100">Enter Topic:</span>
+                            <input className="input100" name="Topic" placeholder="Add Topic for meeting: " onChange={(event) => this.meetingTopic = event.target.value} />
                         </div>
+                        <div className="row">
 
 
-                        <div class="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter Meeting Type">
-                            <span class="label-input100">Meeting Type:*</span>
-                            <input class="input100" type="text" name="name" placeholder=" Enter Meeting Type: " />
-                        </div>
+                            <div className="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter Meeting Called By">
+                                <span className="label-input100">Student Name:*</span>
+                                <input className="input100" type="text" name="name" placeholder=" Enter Student name to meet with: " onChange={(event) => this.meetingWith = event.target.value} />
+                            </div>
 
-                        <div class="wrap-input100 validate-input bg1 rs1-wrap-input100">
-                            <span class="label-input100">Number Of Attendees *</span>
-                            <div>
-                                <select class="js-select2" name="service">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>All were absent</option>
-                                </select>
-                                <div class="dropDownSelect2"></div>
+
+                            <div className="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter Targetted Date" style={{marginLeft: 5 + 'px'}}>
+                                <span className="label-input100">Meeting Date: *</span>
+                                <input className="input100" type="date" name="expected_date" placeholder="Enter Date: " onChange={event => this.meetingDate = event.target.value} />
                             </div>
                         </div>
 
 
-                        <div class="wrap-contact100-form-range">
-                            <span class="label-input100">Progress level *</span>
 
-                            <div class="contact100-form-range-value">
-                                Rate:<span id="Poor"> </span>
-                                <select class="js-select2" name="service">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                    <option>All were absent</option>
-                                </select>
-                            </div>
+                        <div className="container-contact100-form-btn">
 
-                            <div class="contact100-form-range-bar">
-                                <div id="filter-bar"></div>
-                            </div>
-                        </div>
-
-
-                        <div class="wrap-input100 validate-input bg0 rs1-alert-validate" data-validate="Please Type Your Discussion Summery">
-                            <span class="label-input100">Discussion Summery:</span>
-                            <textarea class="input100" name="Discussion Summery" placeholder="Your Discussion Summery here..."></textarea>
-                        </div>
-
-
-                        <div class="wrap-input100 validate-input bg0 rs1-alert-validate" data-validate="Please Type Your Conclusion">
-                            <span class="label-input100">Conclusion:</span>
-                            <textarea class="input100" name="Conclusion" placeholder="Your Conclusion here..."></textarea>
-                        </div>
-
-
-                        <div class="wrap-input100 validate-input bg0 rs1-alert-validate" data-validate="Please Type Items of Action:">
-                            <span class="label-input100">Items of Action:</span>
-                            <textarea class="input100" name="Conclusion" placeholder="Your Items of Action: here..."></textarea>
-                        </div>
-
-
-
-
-                        <div class="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter esponsible Person">
-                            <span class="label-input100">Responsible Person: *</span>
-                            <input class="input100" type="text" name="text" placeholder="Enter esponsible Person " />
-                        </div>
-
-                        <div class="wrap-input100 validate-input bg1 rs1-wrap-input100" data-validate="Enter Targetted Date">
-                            <span class="label-input100">Targetted Date: *</span>
-                            <input class="input100" type="date" name="expected_date" placeholder="Enter Date " />
-                        </div>
-
-
-
-
-
-                        <div class="container-contact100-form-btn">
-                            <button class="contact100-form-btn">
-                                <span>
-                                    Submit
-							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
-                                </span>
+                            <button className="contact100-form-btn" onClick={this.submitForm.bind(this)}>
+                                <span> Submit <i className="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i></span>
                             </button>
                         </div>
+
                     </form>
                 </div>
+            </div>
+        )
+
+    }
+
+    guidGenerator() {
+        var S4 = function () {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        };
+        return (S4() + S4());
+    }
+    submitForm(ev) {
+        ev.preventDefault();
+
+        const { list } = this.state;
+        const meetingDetails = {
+            "meetingNumber": this.guidGenerator(),
+            "meetingTopic": this.meetingTopic,
+            'meetingWith': this.meetingWith,
+            'meetingDate': this.meetingDate,
+
+        }
+        list.push(meetingDetails)
+        if (this.meetingTopic && this.meetingWith && this.meetingDate) {
+            this.setState({ list })
+
+            this.toggleSuccessModal()
+        } else {
+            this.setState({
+                correctInput: false
+            })
+        }
+    }
+    toggleSuccessModal() {
+        this.setState({
+            successModal: !this.state.successModal
+        })
+    }
+    showCreateMeetingForm() {
+        return (
+            <Modal isOpen={this.state.successModal} toggle={this.toggleSuccessModal} className={'modal-success'}>
+                <ModalHeader toggle={this.toggleSuccessModal}>Meeting Minute Form:</ModalHeader>
+                <ModalBody>
+                    {this.form}
+                </ModalBody>
+            </Modal>
+        )
+    }
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardHeader>
+                                <i className="fa fa-align-justify"></i> List of Meetings
+                                <button className="btn btn-sm btn-success"
+                                    type="submit" style={{ marginLeft: 74 + '%' }}
+                                    onClick={this.toggleSuccessModal}
+                                ><i className="glyphicon glyphicon-ok-sign"></i> Set up new meeting</button>
+                            </CardHeader>
+                            <CardBody>
+                                <Table hover bordered striped responsive size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th>Meeting #:</th>
+                                            <th>Topic #:</th>
+                                            <th>With:</th>
+                                            <th>Date:</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.list.map((item, index) => (
+                                            <tr key={index}>
+                                                <td>{item.meetingNumber}</td>
+                                                <td>{item.meetingTopic}</td>
+                                                <td>{item.meetingWith}</td>
+                                                <td>{item.meetingDate}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </Table>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                {this.showCreateMeetingForm()}
             </div>
 
         );
