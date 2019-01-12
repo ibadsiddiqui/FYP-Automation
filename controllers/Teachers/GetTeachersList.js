31d100248fc36a2815624f9e8ff295db621b408a
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-12 16:59:12 +0500

    API updated for NodeJS

--- controllers/Teachers/GetTeachersList.js
+++ controllers/Teachers/GetTeachersList.js
@@ -2,22 +2,17 @@ const Users = require('./../../models/User/User')
 const decodedJWT = require('./../../Helpers/JWTDecoder')
 
 module.exports = async (req, res) => {
+    const username = decodedJWT(req.headers.authorization)
 
+    if (username) {
 
-    if (req.headers.authorization) {
-        const username = decodedJWT(req.headers.authorization)
-        if (username) {
-
-            await Users.getUserByProfession("teacher", (err, teachers) => {
-                if (err) throw err;
-                if (teachers !== null) {
-                    res.sendStatus(200).send(teachers)
-                } else {
-                    res.sendStatus(200).send('none')
-                }
-            })
-        }
-    } else {
-        res.sendStatus(401).status('Please try logging in again')
+        await Users.getUserByProfession("teacher", (err, teachers) => {
+            if (err) throw err;
+            if (teachers !== null) {
+                res.send(teachers)
+            } else {
+                res.send('none')
+            }
+        })
     }
 }
