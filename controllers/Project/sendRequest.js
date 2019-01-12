74661d9f6a21075e57a7b7dea067340a48c0fc5c
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-12 20:23:25 +0500

    controller added for sending request to supervisors

---
+++ controllers/Project/sendRequest.js
@@ -0,0 +1,21 @@
+const RequestModel = require('../../models/User/ProposalRequest');
+
+
+module.exports = async (req, res) => {
+    const username = req.body.supervisorName
+
+    if (username) {
+        const model = {
+            "student_id" : req.body.student_id,
+            "project_name": req.body.project_name,
+            "project_abstract": req.body.project_abstract,
+        } 
+
+        await RequestModel.sendProposalRequest(username, model, (err, requests) => {
+            if(err) throw err;
+            if(requests) {
+                res.status(200).send('success')
+            }
+        })
+    }
+}
