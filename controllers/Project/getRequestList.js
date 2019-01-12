825fa9af238488448abbf58b577ed0a6d1f82247
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-13 00:42:48 +0500

    controller updated

--- controllers/Project/getRequestList.js
+++ controllers/Project/getRequestList.js
@@ -3,18 +3,18 @@ const decodedJWT = require('./../../Helpers/JWTDecoder')
 const User = require('./../../models/User/User')
 module.exports = async (req, res) => {
     const username = decodedJWT(req.headers.authorization)
+
     if (username) {
         await User.getUserByUsername(username, async (err, user) => {
-            console.log(user)
             if (err) throw err;
             if (user) {
-
-                await RequestModel.getProposalRequest(res.name, (err, requests) => {
+                await RequestModel.getProposalRequest(user.name, (err, requests) => {
                     if (err) throw err;
                     if (requests) {
                         res.status(200).send(requests)
                     }
                     if (!requests) {
+
                         res.status(200).send(false)
                     }
                 })
