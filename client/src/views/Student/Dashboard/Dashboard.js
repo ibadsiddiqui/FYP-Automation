b674eacbdfc204d21a75f23c384ce8e743c60f92
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-14 23:15:10 +0500

    URL fixed for meeting minutes

--- client/src/views/Student/Dashboard/Dashboard.js
+++ client/src/views/Student/Dashboard/Dashboard.js
@@ -340,7 +340,7 @@ class Dashboard extends Component {
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="6" lg="3">
-            <a href="#">
+            <Link to="/MeetingMinute">
 
               <Card className="text-white bg-info">
                 <CardBody className="pb-0">
@@ -350,7 +350,7 @@ class Dashboard extends Component {
                   <Line data={cardChartData2} options={cardChartOpts2} height={70} />
                 </div>
               </Card>
-            </a>
+            </Link>
           </Col>
 
           <Col xs="12" sm="6" lg="3">
