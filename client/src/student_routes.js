ae4d5efd1e43fa3c5b25d1b60a5f7f1bf939ec1f
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-01-14 23:14:42 +0500

    route updated for meeting minute list

--- client/src/student_routes.js
+++ client/src/student_routes.js
@@ -7,155 +7,17 @@ function Loading() {
   return <div>Loading...</div>;
 }
 
-// const Breadcrumbs = Loadable({
-//   loader: () => import('./views/Base/Breadcrumbs'),
-//   loading: Loading,
-// });
-
-// const Cards = Loadable({
-//   loader: () => import('./views/Base/Cards'),
-//   loading: Loading,
-// });
-
-// const Carousels = Loadable({
-//   loader: () => import('./views/Base/Carousels'),
-//   loading: Loading,
-// });
-
-// const Collapses = Loadable({
-//   loader: () => import('./views/Base/Collapses'),
-//   loading: Loading,
-// });
-
-// const Dropdowns = Loadable({
-//   loader: () => import('./views/Base/Dropdowns'),
-//   loading: Loading,
-// });
 
 const Forms = Loadable({
   loader: () => import('./views/Base/Forms/Forms'),
   loading: Loading,
 });
 
-// const Jumbotrons = Loadable({
-//   loader: () => import('./views/Base/Jumbotrons'),
-//   loading: Loading,
-// });
-
-// const ListGroups = Loadable({
-//   loader: () => import('./views/Base/ListGroups'),
-//   loading: Loading,
-// });
-
-// const Navbars = Loadable({
-//   loader: () => import('./views/Base/Navbars'),
-//   loading: Loading,
-// });
-
-// const Navs = Loadable({
-//   loader: () => import('./views/Base/Navs'),
-//   loading: Loading,
-// });
-
-// const Paginations = Loadable({
-//   loader: () => import('./views/Base/Paginations'),
-//   loading: Loading,
-// });
-
-// const Popovers = Loadable({
-//   loader: () => import('./views/Base/Popovers'),
-//   loading: Loading,
-// });
-
-// const ProgressBar = Loadable({
-//   loader: () => import('./views/Base/ProgressBar'),
-//   loading: Loading,
-// });
-
-// const Switches = Loadable({
-//   loader: () => import('./views/Base/Switches'),
-//   loading: Loading,
-// });
-
-// const Tables = Loadable({
-//   loader: () => import('./views/Base/Tables'),
-//   loading: Loading,
-// });
-
-// const Tabs = Loadable({
-//   loader: () => import('./views/Base/Tabs'),
-//   loading: Loading,
-// });
-
-// const Tooltips = Loadable({
-//   loader: () => import('./views/Base/Tooltips'),
-//   loading: Loading,
-// });
-
-// const BrandButtons = Loadable({
-//   loader: () => import('./views/Buttons/BrandButtons'),
-//   loading: Loading,
-// });
-
-// const ButtonDropdowns = Loadable({
-//   loader: () => import('./views/Buttons/ButtonDropdowns'),
-//   loading: Loading,
-// });
-
-// const ButtonGroups = Loadable({
-//   loader: () => import('./views/Buttons/ButtonGroups'),
-//   loading: Loading,
-// });
-
-// const Buttons = Loadable({
-//   loader: () => import('./views/Buttons/Buttons'),
-//   loading: Loading,
-// });
-
-// const Charts = Loadable({
-//   loader: () => import('./views/Charts'),
-//   loading: Loading,
-// });
 
 const Dashboard = Loadable({
   loader: () => import('./views/Student/Dashboard/Dashboard'),
   loading: Loading,
 });
-
-// const CoreUIIcons = Loadable({
-//   loader: () => import('./views/Icons/CoreUIIcons'),
-//   loading: Loading,
-// });
-
-// const Flags = Loadable({
-//   loader: () => import('./views/Icons/Flags'),
-//   loading: Loading,
-// });
-
-// const FontAwesome = Loadable({
-//   loader: () => import('./views/Icons/FontAwesome'),
-//   loading: Loading,
-// });
-
-// const SimpleLineIcons = Loadable({
-//   loader: () => import('./views/Icons/SimpleLineIcons'),
-//   loading: Loading,
-// });
-
-// const Alerts = Loadable({
-//   loader: () => import('./views/Notifications/Alerts'),
-//   loading: Loading,
-// });
-
-// const Badges = Loadable({
-//   loader: () => import('./views/Notifications/Badges'),
-//   loading: Loading,
-// });
-
-// const Modals = Loadable({
-//   loader: () => import('./views/Notifications/Modals'),
-//   loading: Loading,
-// });
 
 const Profile = Loadable({
   loader: () => import('./views/Student/Profile'),
@@ -178,18 +40,9 @@ const Templates = Loadable({
   loading: Loading,
 });
 
-// const Widgets = Loadable({
-//   loader: () => import('./views/Widgets/Widgets'),
-//   loading: Loading,
-// });
 
-const Users = Loadable({
-  loader: () => import('./views/Users/Users'),
-  loading: Loading,
-});
-
-const User = Loadable({
-  loader: () => import('./views/Users/User'),
+const MeetingMinutes = Loadable({
+  loader: () => import('./views/Student/Meeting_Minutes'),
   loading: Loading,
 });
 
@@ -203,7 +56,7 @@ const student_routes = [
   { path: '/Sharing', exact: true, name: 'Sharing With Supervisor', component: Sharing },
   { path: '/Projects', exact: true, name: 'Over All Projects', component: Projects },
   { path: '/Templates', exact: true, name: 'Templates For FYP', component: Templates },
-  // { path: '/base', exact: true, name: 'Base', component: Cards },
+  { path: '/MeetingMinute', exact: true, name: 'Meeting Minute', component: MeetingMinutes },
   // { path: '/base/cards', name: 'Cards', component: Cards },
   { path: '/base/forms', name: 'Forms', component: Forms },
   // { path: '/base/switches', name: 'Switches', component: Switches },
