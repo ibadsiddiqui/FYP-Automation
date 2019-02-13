fed4bece706ff1957cfa2a016d59ba3a6973c469
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-02-13 09:38:18 +0500

    Update request model for proposal request.

--- controllers/Project/sendRequest.js
+++ controllers/Project/sendRequest.js
@@ -1,4 +1,5 @@
 const RequestModel = require('../../models/User/ProposalRequest');
+const decodedJWT = require('../../Helpers/JWTDecoder')
 
 
 module.exports = async (req, res) => {
@@ -6,17 +7,19 @@ module.exports = async (req, res) => {
 
     if (username) {
         const model = {
-            "student_id" : req.body.student_id,
+            "student_id": decodedJWT(req.body.student_id),
             "project_name": req.body.project_name,
             "project_abstract": req.body.project_abstract,
-            "student_name" : req.body.student_name,
-            "accepted" : false
-        } 
+            // "student_name": req.body.student_name,
+            "accepted": false
+        }
 
         await RequestModel.sendProposalRequest(username, model, (err, requests) => {
-            if(err) throw err;
-            if(requests) {
+            if (err) throw err;
+            if (requests) {
                 res.status(200).send('success')
+            }else {
+                res.status(500).send('internal server error')
             }
         })
     }
